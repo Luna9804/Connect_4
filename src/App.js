cd8dfaf401c27a0d96d3774abdd0d632c5640322
Luna9804 <56940000+Luna9804@users.noreply.github.com>
2022-05-23 20:37:57 -0400

    continue working on the board

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 export default function App() {
   const [playerTurn, changePlayerTurn] = useState("red");
+  const [tokens, changeTokens] = useState([]);
 
   function togglePlayerTurn() {
     if (playerTurn === "red") {
@@ -12,16 +13,22 @@ export default function App() {
     }
   }
 
+  function addToken() {}
+
   return (
     <div className="App">
       <h1>Hello My React App</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <div className = "Column">
-      <div className = " BlueBox"
-          onClick = {() => togglePlayerTurn()}>
-        <div className={`${playerTurn} Circle`}></div>
-      </div> 
-      <div className="Bluebox"></div>
+      <div className="Column" onClick={addToken}>
+        <div className=" BlueBox" onClick={() => togglePlayerTurn()}>
+          <div className={`${playerTurn} Circle`}></div>
+        </div>{" "}
+        <div className=" BlueBox" onClick={() => togglePlayerTurn()}>
+          <div className={`${playerTurn} Circle`}></div>
+        </div>
+        <div className=" BlueBox" onClick={() => togglePlayerTurn()}>
+          <div className={`${playerTurn} Circle`}></div>
+        </div>
       </div>
       <p>the color is {playerTurn}</p>
     </div>
